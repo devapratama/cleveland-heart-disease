30354a8d6e71654678ef1cedee6a97aa4b8e1445
Ahmad Sabil Deva Pratama <74589971+devapratama@users.noreply.github.com>
2024-01-16 21:56:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 
 # Load your trained Random Forest model
 model = pickle.load(open('rf.pkl', 'rb'))
